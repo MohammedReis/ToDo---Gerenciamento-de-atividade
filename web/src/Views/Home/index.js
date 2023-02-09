8b3c9483024ddc472a45e1847a2afccf7b03ca84
MohammedReis <70699636+MohammedReis@users.noreply.github.com>
2023-02-09 09:54:45 -0300

    Feat: Componet FilterCard

--- web/src/Views/Home/index.js
+++ web/src/Views/Home/index.js
@@ -1,15 +1,35 @@
+import {useState} from 'react';
 import * as S from'./styles'
 
 //Componentes
 import Header from "../../Components/Header";
 import Footer from '../../Components/Footer';
+import FilterCard from '../../Components/FilterCard';
 
 
 
 function Home() {
+    const[filterActived, setFilterActived] = useState('today')
     return (
         <S.Container>
             <Header/>
+            <S.FilterArea>
+                <button type="button"onClick={()=> setFilterActived("all")} >
+                <FilterCard title="Todos"   actived={filterActived == 'all'}/>
+                </button>
+                <button type="button"onClick={()=> setFilterActived("today")} >
+                <FilterCard title="Hoje"    actived={filterActived == 'today'}/>
+                </button>
+                <button type="button"onClick={()=> setFilterActived("week")} >
+                <FilterCard title="Semana"  actived={filterActived == 'week'}/>
+                </button>
+                <button type="button"onClick={()=> setFilterActived("month")} >
+                <FilterCard title="Mês"     actived={filterActived == 'month'}/>
+                </button>
+                <button type="button"onClick={()=> setFilterActived("year")} >
+                <FilterCard title="Ano"     actived={filterActived == 'year'}/>
+                </button>
+            </S.FilterArea>
             <Footer/>
         </S.Container>
     );
